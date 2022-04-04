761262091636bb67dc019b3afa6a68c03a3499cd
Sergio <theserdzo@gmail.com>
2022-04-04 19:02:53 +0500

    pulish

---
+++ js/scrt.js
@@ -0,0 +1,12 @@
+        WebFont.load({
+            google: {
+                families: ["Italiana:regular", "Syne:regular,500,600,700:latin,latin-ext"]
+            }
+        });
+
+        <!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif]-->
+        ! function(o, c) {
+            var n = c.documentElement,
+                t = " w-mod-";
+            n.className += t + "js", ("ontouchstart" in o || o.DocumentTouch && c instanceof DocumentTouch) && (n.className += t + "touch")
+        }(window, document);
